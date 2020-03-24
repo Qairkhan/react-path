b56f8babca40d08f1a332a68340bbd868ecaa5f7
Qairkhan <shyngys94@gmail.com>
2020-03-24 18:28:46 +0300

    more components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
     <div>
-        My post
-        <div>new post</div>
-        <div className={s.posts}>
-        <div className={s.item}>post 1</div>
-        <div className={s.item}>post 2</div>
-        </div>
+      My post
+      <div>
+        <textarea></textarea>
+        <button>Add post</button>
       </div>
+      <Post />
+      <Post />
+    </div>
   );
 };
 
