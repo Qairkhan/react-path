eda17e0bdad01a3bc73efaf329b86d2285491ec7
Qairkhan <shyngys94@gmail.com>
2020-04-26 19:16:42 +0300

    fixed

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 
 import {ROUTES} from "./../core/constants"
 
-let mapStateToPropsForRedirect = (state) => ({
+const mapStateToPropsForRedirect = (state) => ({
     isAuth: state.auth.isAuth
   }); 
 
