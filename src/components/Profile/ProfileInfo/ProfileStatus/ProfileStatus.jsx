88837f5442237d56fff31b7f589aa382cefc4140
Qairkhan <shyngys94@gmail.com>
2020-04-27 13:42:26 +0300

    fixed

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -2,15 +2,27 @@ import React from "react";
 
 import s from "./ProfileStatus.module.css";
 
-class ProfileStatus extends React.Component{
+class ProfileStatus extends React.Component {
+  state = {
+    editMode: false,
+  };
+
   render() {
-  return (
-    <div >
-      <div><span>{this.props.status}</span></div>
-      <div><input value={this.props.status} /></div>
-    </div>
-  );
+    return (
+      <div>
+        {this.state.editMode && (
+          <div>
+            <span>{this.props.status}</span>
+          </div>
+        )}
+        {this.state.editMode && (
+          <div>
+            <input value={this.props.status} />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-};
 
 export default ProfileStatus;
