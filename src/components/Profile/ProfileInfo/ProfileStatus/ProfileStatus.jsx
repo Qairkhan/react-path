30a6a46a88b2d114698dd177c12f3447092af3ee
Qairkhan <shyngys94@gmail.com>
2020-04-27 13:33:14 +0300

    add texarea

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 
-import { Preloader } from "../../common/Preloader/Preloader";
-
 import s from "./ProfileStatus.module.css";
 
-const PROFILE_LINK =
-  "https://animal-wallpaper.com/wallpaper/minimalist-background-hd-For-Background-HD-Wallpaper.jpg";
-
 const ProfileStatus = (props) => {
-  console.log("profileInfo", props);
-  if (!props.profile) {
-    return <Preloader />;
-  }
   return (
-    <div className={s.content}>
-      <div>
-        <img src={PROFILE_LINK} className={s.contentImg} />
-      </div>
-      <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large} />
-        ava+description
-      </div>
+    <div >
+      <div><span>{props.status}</span></div>
+      <div><input value={props.status} /></div>
     </div>
   );
 };
