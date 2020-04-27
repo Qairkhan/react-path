7ad8c293668e1782849c8e03f045af20525beef7
Qairkhan <shyngys94@gmail.com>
2020-04-27 13:40:44 +0300

    ProfileStatus fix

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import s from "./ProfileStatus.module.css";
 
-const ProfileStatus = (props) => {
+class ProfileStatus extends React.Component{
+  render() {
   return (
     <div >
-      <div><span>{props.status}</span></div>
-      <div><input value={props.status} /></div>
+      <div><span>{this.props.status}</span></div>
+      <div><input value={this.props.status} /></div>
     </div>
   );
+}
 };
 
 export default ProfileStatus;
