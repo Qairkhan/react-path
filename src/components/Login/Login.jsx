4a376c5d3fca0cc602896ff82b7c0d69c993ea92
Qairkhan <shyngys94@gmail.com>
2020-04-27 16:12:34 +0300

    settings login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { I18N } from "../../core/constants";
 
-const Login = (props) => {
-    return <h1>{I18N.EN.LOGIN}</h1>
-}
+const LoginForm = (props) => {
+  return (
+    <form action="">
+      <div>
+        <input placeholder={I18N.EN.LOGIN} />
+      </div>
+      <div>
+        <input placeholder={"Pass"} />
+      </div>
+      <div>
+        <input type={"checkbox"} /> remember me
+      </div>
+      <div>
+        <button>{I18N.EN.LOGIN}</button>
+      </div>
+    </form>
+  );
+};
 
-export {Login};
+const Login = (props) => {
+  return (
+    <div>
+      <h1>{I18N.EN.LOGIN}</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export { Login };
