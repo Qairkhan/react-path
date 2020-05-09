9808a7dd5d60e9cb9af6e5f80ae79b6ab26e580e
Qairkhan <shyngys94@gmail.com>
2020-05-09 16:30:30 +0300

    update imports

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHook.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHook.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import s from "./ProfileStatus.module.css";
 
 const ProfileStatusWithHooks = (props) => {
-
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
-
 
   const activeteEditMode = () => {
     setEditMode(true);
@@ -15,18 +12,16 @@ const ProfileStatusWithHooks = (props) => {
 
   const deactiveteEditMode = () => {
     setEditMode(false);
-    // props.updateStatus(this.state.status);
+    props.updateStatus(status);
   };
 
   const onStatusChange = (e) => {
-    setStatus(e.currentTarget.value );
+    setStatus(e.currentTarget.value);
   };
 
   let renderViewMode = () => (
     <div>
-      <span onDoubleClick={activeteEditMode}>
-        {props.status || "----"}
-      </span>
+      <span onDoubleClick={activeteEditMode}>{props.status || "----"}</span>
     </div>
   );
 
@@ -36,19 +31,14 @@ const ProfileStatusWithHooks = (props) => {
         onChange={onStatusChange}
         autoFocus={true}
         onBlur={deactiveteEditMode}
-        value={state.status}
+        value={status}
       />
     </div>
   );
 
-  let renderMode = () =>
-    editMode ? renderEditMode() : renderViewMode();
+  let renderMode = () => (editMode ? renderEditMode() : renderViewMode());
 
-  
-    return (
-    <div>{renderMode()}</div>
-    )
-  
-}
+  return <div>{renderMode()}</div>;
+};
 
-export {ProfileStatusWithHooks};
+export { ProfileStatusWithHooks };
