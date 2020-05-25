35f5432a41ca1e1ba06308cd2ae9f7ff310fd39e
Qairkhan <shyngys94@gmail.com>
2020-05-25 19:10:01 +0300

    fix

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -19,40 +19,41 @@ const Contact = ({ contactTitle, contactValue }) => {
   );
 };
 
-const ProfileDescription = ({profile}) => {
- return <div>
-  {" "}
-  <div>
-    <b>Full Name:</b> {profile.fullName}
-  </div>
-  <div>
-    <b>Looking for a job:</b>{" "}
-    {profile.lookingForAJob ? "yes" : "no"}
-  </div>{" "}
-  {profile.lookingForAJob && (
+const ProfileDescription = ({ profile }) => {
+  return (
     <div>
       {" "}
-      <b>My professional skills:</b>
-      {profile.lookingForAJobDescription}{" "}
+      <div>
+        <b>Full Name:</b> {profile.fullName}
+      </div>
+      <div>
+        <b>Looking for a job:</b> {profile.lookingForAJob ? "yes" : "no"}
+      </div>{" "}
+      {profile.lookingForAJob && (
+        <div>
+          {" "}
+          <b>My professional skills:</b>
+          {profile.lookingForAJobDescription}{" "}
+        </div>
+      )}
+      <div>
+        <b>About me:</b> {profile.aboutMe}
+      </div>
+      <div>
+        <b>Contacts:</b>{" "}
+        {Object.keys(profile.contacts).map((key) => {
+          return (
+            <Contact
+              key={key}
+              contactTitle={key}
+              contactValue={profile.contacts[key]}
+            />
+          );
+        })}
+      </div>
     </div>
-  )}
-  <div>
-    <b>About me:</b> {profile.aboutMe}
-  </div>
-  <div>
-    <b>Contacts:</b>{" "}
-    {Object.keys(profile.contacts).map((key) => {
-      return (
-        <Contact
-          key={key}
-          contactTitle={key}
-          contactValue={profile.contacts[key]}
-        />
-      );
-    })}
-  </div>
-</div>
-}
+  );
+};
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -79,7 +80,7 @@ const ProfileInfo = (props) => {
         status={`${props.status}`}
         updateStatus={props.updateStatus}
       />
-<ProfileDescription profile={props.profile}/>
+      <ProfileDescription profile={props.profile} />
     </div>
   );
 };
