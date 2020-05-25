1d9eeb4ecd715fbb6370272034c43dfbbda0720b
Qairkhan <shyngys94@gmail.com>
2020-05-25 19:32:08 +0300

    edit mode

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -55,6 +55,42 @@ const ProfileDescription = ({ profile }) => {
   );
 };
 
+const ProfileDescriptionForm = ({ profile }) => {
+  return (
+    <div>
+      {" "}
+      <div>
+        <b>Full Name:</b> {profile.fullName}
+      </div>
+      <div>
+        <b>Looking for a job:</b> {profile.lookingForAJob ? "yes" : "no"}
+      </div>{" "}
+      {profile.lookingForAJob && (
+        <div>
+          {" "}
+          <b>My professional skills:</b>
+          {profile.lookingForAJobDescription}{" "}
+        </div>
+      )}
+      <div>
+        <b>About me:</b> {profile.aboutMe}
+      </div>
+      <div>
+        <b>Contacts:</b>{" "}
+        {Object.keys(profile.contacts).map((key) => {
+          return (
+            <Contact
+              key={key}
+              contactTitle={key}
+              contactValue={profile.contacts[key]}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
@@ -80,7 +116,8 @@ const ProfileInfo = (props) => {
         status={`${props.status}`}
         updateStatus={props.updateStatus}
       />
-      <ProfileDescription profile={props.profile} />
+      { editMode ? <ProfileDescription profile={props.profile}/> : <ProfileDescriptionForm profile={props.profile}
+       /> }
     </div>
   );
 };
