ee207b5f651ffe91cde2195900b3b845bdba67f2
Qairkhan <shyngys94@gmail.com>
2020-05-26 15:48:36 +0300

    fix

--- src/components/Profile/ProfileInfo/ProfileDescriptionForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDescriptionForm.jsx
@@ -1,7 +1,41 @@
 import React, { useState, useEffect } from "react";
 
 const ProfileDescriptionForm = ({ profile }) => {
-    return <div>Form</div>;
+    return    <form>
+    {isOwner && (
+      <div onClick={goToEditMode}>
+        <button>edit</button>
+      </div>
+    )}{" "}
+    <div>
+      <b>Full Name:</b> {profile.fullName}
+    </div>
+    <div>
+      <b>Looking for a job:</b> {profile.lookingForAJob ? "yes" : "no"}
+    </div>{" "}
+    {profile.lookingForAJob && (
+      <div>
+        {" "}
+        <b>My professional skills:</b>
+        {profile.lookingForAJobDescription}{" "}
+      </div>
+    )}
+    <div>
+      <b>About me:</b> {profile.aboutMe}
+    </div>
+    <div>
+      <b>Contacts:</b>{" "}
+      {Object.keys(profile.contacts).map((key) => {
+        return (
+          <Contact
+            key={key}
+            contactTitle={key}
+            contactValue={profile.contacts[key]}
+          />
+        );
+      })}
+    </div>
+  </form>;
   };
 
   export {ProfileDescriptionForm} 
