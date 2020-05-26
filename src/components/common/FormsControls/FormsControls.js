b0b5107976c05d9d62ef34d4267980dce519f00c
Qairkhan <shyngys94@gmail.com>
2020-05-26 16:03:47 +0300

    fix FormControl

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { Field } from "redux-form";
 
-const TextareaCastom = ({input, meta, ...props}) => {
-    return (
-        <div>
-            <textarea {...input} {...props}/>
-        </div>
-        )
-}
+const TextareaCastom = ({ input, meta, ...props }) => {
+  return (
+    <div>
+      <textarea {...input} {...props} />
+    </div>
+  );
+};
 
+const createField = (
+  placeholder,
+  name,
+  validators,
+  component,
+  props = {},
+  text = ""
+) => (
+  <div>
+    <Field
+      placeholder={placeholder}
+      name={name}
+      validate={validators}
+      component={component}
+      {...props}
+    />{" "}
+    {text}
+  </div>
+);
 
-
-export {TextareaCastom}
+export { TextareaCastom, createField };
