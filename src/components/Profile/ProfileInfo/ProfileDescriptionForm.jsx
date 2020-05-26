380b1beba65ce3fe87cab5f959e95624744f155a
Qairkhan <shyngys94@gmail.com>
2020-05-26 15:49:41 +0300

    fix

--- src/components/Profile/ProfileInfo/ProfileDescriptionForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDescriptionForm.jsx
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from "react";
 
 const ProfileDescriptionForm = ({ profile }) => {
-    return    <form>
-    {isOwner && (
+  return (
+    <form>
       <div onClick={goToEditMode}>
-        <button>edit</button>
+        <button>save</button>
       </div>
-    )}{" "}
-    <div>
-      <b>Full Name:</b> {profile.fullName}
-    </div>
-    <div>
-      <b>Looking for a job:</b> {profile.lookingForAJob ? "yes" : "no"}
-    </div>{" "}
-    {profile.lookingForAJob && (
       <div>
-        {" "}
-        <b>My professional skills:</b>
-        {profile.lookingForAJobDescription}{" "}
+        <b>Full Name:</b> {profile.fullName}
       </div>
-    )}
-    <div>
-      <b>About me:</b> {profile.aboutMe}
-    </div>
-    <div>
-      <b>Contacts:</b>{" "}
-      {Object.keys(profile.contacts).map((key) => {
-        return (
-          <Contact
-            key={key}
-            contactTitle={key}
-            contactValue={profile.contacts[key]}
-          />
-        );
-      })}
-    </div>
-  </form>;
-  };
+      <div>
+        <b>Looking for a job:</b> {profile.lookingForAJob ? "yes" : "no"}
+      </div>{" "}
+      {profile.lookingForAJob && (
+        <div>
+          {" "}
+          <b>My professional skills:</b>
+          {profile.lookingForAJobDescription}{" "}
+        </div>
+      )}
+      <div>
+        <b>About me:</b> {profile.aboutMe}
+      </div>
+      <div>
+        <b>Contacts:</b>{" "}
+        {Object.keys(profile.contacts).map((key) => {
+          return (
+            <Contact
+              key={key}
+              contactTitle={key}
+              contactValue={profile.contacts[key]}
+            />
+          );
+        })}
+      </div>
+    </form>
+  );
+};
 
-  export {ProfileDescriptionForm} 
+export { ProfileDescriptionForm };
